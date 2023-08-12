78064c6081659bc42a2912a49e8c90c0e6bb8507
ActuaryEmma <emmanyakio@gmail.com>
2023-08-13 02:29:23 +0300

    Update

--- console.py
+++ console.py
@@ -13,29 +13,42 @@ class HBNBCommand(cmd.Cmd):
     prompt = '(hbnb)'
     class_list = ["BaseModel"]
 
+    @staticmethod
+    def error_message(caller, arg):
+        list_message = ["** class name missing **",
+                        "** class doesn't exist **",
+                        "** instance id missing **",
+                        "** no instance found **"]
+        if not arg:
+            print(list_message[0])
+        elif caller == 'show' and arg.split()[0] not in HBNBCommand.class_list:
+            print(list_message[1])
+        elif caller == 'create' and arg not in HBNBCommand.class_list:
+            print(list_message[1])
+        elif caller == "show" and len(arg.split()) < 2:
+            print(list_message[2])
+        elif caller == "destroy" and len(arg.split()) < 2:
+            print(list_message[2])
+        else:
+            return 0
+
+        
     def do_create(self, arg):
         """Creates a new instance of BaseModel, saves it"""
-        if not arg:
-            print("** class name missing **")
-        elif arg not in HBNBCommand.class_list:
-            print("** class doesn't exist **")
+        if HBNBCommand.error_message("create", arg) == None:
+            return
         else:
             base = BaseModel()
             base.save()
             base_id = base.id
             print(base_id)
 
-    def do_show(self, line):
+    def do_show(self, arg):
         """string representation of an instance based on the class name / id"""
-        args = line.split()
         objects = {}
-        found_object = {}
-        if not args:
-            print("** class name missing **")
-        elif args[0] not in HBNBCommand.class_list:
-            print("** class doesn't exist **")
-        elif len(args) < 2:
-            print("** instance id missing **")
+        args = arg.split()
+        if HBNBCommand.error_message("show", arg) == None:
+            return
         else:
             objects = FileStorage().all()
             key = f"{args[0]}.{args[1]}"
@@ -43,6 +56,14 @@ class HBNBCommand(cmd.Cmd):
                 print("** no instance found **")
             else:
                 print(objects.get(key))
+
+    def do_destroy(self, arg):
+        if not arg:
+            print("** class name missing **")
+        elif arg[0] not in HBNBCommand.class_list:
+            print("** class doesn't exist **")
+        elif len(arg) < 2:
+            print("** instance id missing **")
 
     def emptyline(self):
         """empty file should execute nothing"""
@@ -62,12 +83,5 @@ class HBNBCommand(cmd.Cmd):
 
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        # Read commands from stdin
-        commands = sys.stdin.readlines()
-        my_cmd = HBNBCommand(stdin=commands)
-        my_cmd.cmdloop()
-    else:
-        # Interactive mode
-        my_cmd = HBNBCommand()
-        my_cmd.cmdloop()
+    my_cmd = HBNBCommand()
+    my_cmd.cmdloop()
